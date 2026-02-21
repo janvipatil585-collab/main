58ba2c6407393a664a20144370cb1b562ba58ba9
janvipatil585-collab <janvipatil585@gmail.com>
2026-02-21 15:43:20 +0530

    cpp code to take input from the user

---
+++ input.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int apples;
+    cout<<"Enter the number of apples you have: ";
+    cin>>apples;
+    cout<<"You have "<<apples<<" apples.";
+    return 0;
+}
